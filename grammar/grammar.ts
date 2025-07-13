0bea22d82d5e5fdd198eb25edfecebb921e645a5
Jason Kuhrt <jasonkuhrt@me.com>
2025-07-13 19:31:50 -0400

    chore: fix type annotations

--- grammar/grammar.ts
+++ grammar/grammar.ts
@@ -1,33 +1,21 @@
-/// <reference types="tree-sitter-cli/dsl" />
-
-// Global functions available in tree-sitter grammar context
-declare const grammar: any
-declare const choice: any
-declare const optional: any
-declare const repeat1: any
-declare const repeat: any
-declare const seq: any
-declare const prec: any
-declare const token: any
-
 export default grammar({
   name: 'graphql',
 
-  extras: ($) => [/[\s\uFEFF\u0009\u0020\u000A\u000D]/, $.comma, $.comment],
+  extras: ($: any) => [/[\s\uFEFF\u0009\u0020\u000A\u000D]/, $.comma, $.comment],
 
   rules: {
-    source_file: ($) => $.document,
-    document: ($) => repeat1($.definition),
-    definition: ($) =>
+    source_file: ($: any) => $.document,
+    document: ($: any) => repeat1($.definition),
+    definition: ($: any) =>
       choice(
         $.executable_definition,
         $.type_system_definition,
         $.type_system_extension,
       ),
-    executable_definition: ($) => choice($.operation_definition, $.fragment_definition),
-    type_system_definition: ($) => choice($.schema_definition, $.type_definition, $.directive_definition),
-    type_system_extension: ($) => choice($.schema_extension, $.type_extension),
-    schema_definition: ($) =>
+    executable_definition: ($: any) => choice($.operation_definition, $.fragment_definition),
+    type_system_definition: ($: any) => choice($.schema_definition, $.type_definition, $.directive_definition),
+    type_system_extension: ($: any) => choice($.schema_extension, $.type_extension),
+    schema_definition: ($: any) =>
       seq(
         optional($.description),
         'schema',
@@ -36,7 +24,7 @@ export default grammar({
         repeat1($.root_operation_type_definition),
         '}',
       ),
-    schema_extension: ($) =>
+    schema_extension: ($: any) =>
       seq(
         'extend',
         'schema',
@@ -45,7 +33,7 @@ export default grammar({
         $.root_operation_type_definition,
         '}',
       ),
-    type_extension: ($) =>
+    type_extension: ($: any) =>
       choice(
         $.scalar_type_extension,
         $.object_type_extension,
@@ -54,8 +42,8 @@ export default grammar({
         $.enum_type_extension,
         $.input_object_type_extension,
       ),
-    scalar_type_extension: ($) => seq('extend', 'scalar', $.name, $.directives),
-    object_type_extension: ($) =>
+    scalar_type_extension: ($: any) => seq('extend', 'scalar', $.name, $.directives),
+    object_type_extension: ($: any) =>
       prec.right(
         choice(
           seq(
@@ -75,7 +63,7 @@ export default grammar({
           ),
         ),
       ),
-    interface_type_extension: ($) =>
+    interface_type_extension: ($: any) =>
       prec.right(
         choice(
           seq(
@@ -95,7 +83,7 @@ export default grammar({
           ),
         ),
       ),
-    union_type_extension: ($) =>
+    union_type_extension: ($: any) =>
       prec.right(
         choice(
           seq(
@@ -108,7 +96,7 @@ export default grammar({
           seq('extend', 'union', $.name, optional($.directives)),
         ),
       ),
-    enum_type_extension: ($) =>
+    enum_type_extension: ($: any) =>
       prec.right(
         choice(
           seq(
@@ -121,7 +109,7 @@ export default grammar({
           seq('extend', 'enum', $.name, optional($.directives)),
         ),
       ),
-    input_object_type_extension: ($) =>
+    input_object_type_extension: ($: any) =>
       prec.right(
         choice(
           seq(
@@ -134,16 +122,16 @@ export default grammar({
           seq('extend', 'input', $.name, optional($.directives)),
         ),
       ),
-    input_fields_definition: ($) => seq('{', repeat1($.input_value_definition), '}'),
-    enum_values_definition: ($) => seq('{', repeat1($.enum_value_definition), '}'),
-    enum_value_definition: ($) => seq(optional($.description), $.enum_value, optional($.directives)),
-    implements_interfaces: ($) =>
+    input_fields_definition: ($: any) => seq('{', repeat1($.input_value_definition), '}'),
+    enum_values_definition: ($: any) => seq('{', repeat1($.enum_value_definition), '}'),
+    enum_value_definition: ($: any) => seq(optional($.description), $.enum_value, optional($.directives)),
+    implements_interfaces: ($: any) =>
       choice(
         seq($.implements_interfaces, '&', $.named_type),
         seq('implements', optional('&'), $.named_type),
       ),
-    fields_definition: ($) => seq('{', repeat1($.field_definition), '}'),
-    field_definition: ($) =>
+    fields_definition: ($: any) => seq('{', repeat1($.field_definition), '}'),
+    field_definition: ($: any) =>
       seq(
         optional($.description),
         $.name,
@@ -152,8 +140,8 @@ export default grammar({
         $.type,
         optional($.directives),
       ),
-    arguments_definition: ($) => seq('(', repeat1($.input_value_definition), ')'),
-    input_value_definition: ($) =>
+    arguments_definition: ($: any) => seq('(', repeat1($.input_value_definition), ')'),
+    input_value_definition: ($: any) =>
       seq(
         optional($.description),
         $.name,
@@ -162,14 +150,14 @@ export default grammar({
         optional($.default_value),
         optional($.directives),
       ),
-    default_value: ($) => seq('=', $.value),
-    union_member_types: ($) =>
+    default_value: ($: any) => seq('=', $.value),
+    union_member_types: ($: any) =>
       choice(
         seq($.union_member_types, '|', $.named_type),
         seq('=', optional('|'), $.named_type),
       ),
-    root_operation_type_definition: ($) => seq($.operation_type, ':', $.named_type),
-    operation_definition: ($) =>
+    root_operation_type_definition: ($: any) => seq($.operation_type, ':', $.named_type),
+    operation_definition: ($: any) =>
       choice(
         $.selection_set,
         seq(
@@ -180,8 +168,8 @@ export default grammar({
           $.selection_set,
         ),
       ),
-    operation_type: ($) => choice('query', 'mutation', 'subscription'),
-    type_definition: ($) =>
+    operation_type: (_$: any) => choice('query', 'mutation', 'subscription'),
+    type_definition: ($: any) =>
       choice(
         $.scalar_type_definition,
         $.object_type_definition,
@@ -190,11 +178,11 @@ export default grammar({
         $.enum_type_definition,
         $.input_object_type_definition,
       ),
-    scalar_type_definition: ($) =>
+    scalar_type_definition: ($: any) =>
       prec.right(
         seq(optional($.description), 'scalar', $.name, optional($.directives)),
       ),
-    object_type_definition: ($) =>
+    object_type_definition: ($: any) =>
       prec.right(
         seq(
           optional($.description),
@@ -205,7 +193,7 @@ export default grammar({
           optional($.fields_definition),
         ),
       ),
-    interface_type_definition: ($) =>
+    interface_type_definition: ($: any) =>
       prec.right(
         seq(
           optional($.description),
@@ -216,7 +204,7 @@ export default grammar({
           optional($.fields_definition),
         ),
       ),
-    union_type_definition: ($) =>
+    union_type_definition: ($: any) =>
       prec.right(
         seq(
           optional($.description),
@@ -226,7 +214,7 @@ export default grammar({
           optional($.union_member_types),
         ),
       ),
-    enum_type_definition: ($) =>
+    enum_type_definition: ($: any) =>
       prec.right(
         seq(
           optional($.description),
@@ -236,7 +224,7 @@ export default grammar({
           optional($.enum_values_definition),
         ),
       ),
-    input_object_type_definition: ($) =>
+    input_object_type_definition: ($: any) =>
       prec.right(
         seq(
           optional($.description),
@@ -246,8 +234,8 @@ export default grammar({
           optional($.input_fields_definition),
         ),
       ),
-    variable_definitions: ($) => seq('(', repeat1($.variable_definition), ')'),
-    variable_definition: ($) =>
+    variable_definitions: ($: any) => seq('(', repeat1($.variable_definition), ')'),
+    variable_definition: ($: any) =>
       seq(
         $.variable,
         ':',
@@ -256,9 +244,9 @@ export default grammar({
         optional($.directives),
         optional($.comma),
       ),
-    selection_set: ($) => seq('{', repeat1($.selection), '}'),
-    selection: ($) => choice($.field, $.inline_fragment, $.fragment_spread),
-    field: ($) =>
+    selection_set: ($: any) => seq('{', repeat1($.selection), '}'),
+    selection: ($: any) => choice($.field, $.inline_fragment, $.fragment_spread),
+    field: ($: any) =>
       seq(
         optional($.alias),
         $.name,
@@ -266,10 +254,10 @@ export default grammar({
         optional($.directive),
         optional($.selection_set),
       ),
-    alias: ($) => seq($.name, ':'),
-    arguments: ($) => seq('(', repeat1($.argument), ')'),
-    argument: ($) => seq($.name, ':', $.value),
-    value: ($) =>
+    alias: ($: any) => seq($.name, ':'),
+    arguments: ($: any) => seq('(', repeat1($.argument), ')'),
+    argument: ($: any) => seq($.name, ':', $.value),
+    value: ($: any) =>
       choice(
         $.variable,
         $.string_value,
@@ -281,14 +269,14 @@ export default grammar({
         $.list_value,
         $.object_value,
       ),
-    variable: ($) => seq('$', $.name),
-    string_value: ($) =>
+    variable: ($: any) => seq('$', $.name),
+    string_value: (_$: any) =>
       choice(
         seq('"""', /([^"]|\n|""?[^"])*/, '"""'),
         seq('"', /[^"\\\n]*/, '"'),
       ),
-    int_value: ($) => /-?(0|[1-9][0-9]*)/,
-    float_value: ($) =>
+    int_value: (_$: any) => /-?(0|[1-9][0-9]*)/,
+    float_value: (_$: any) =>
       token(
         seq(
           /-?(0|[1-9][0-9]*)/,
@@ -299,14 +287,14 @@ export default grammar({
           ),
         ),
       ),
-    boolean_value: ($) => choice('true', 'false'),
-    null_value: ($) => 'null',
-    enum_value: ($) => $.name,
-    list_value: ($) => seq('[', repeat($.value), ']'),
-    object_value: ($) => seq('{', repeat($.object_field), '}'),
-    object_field: ($) => seq($.name, ':', $.value, optional($.comma)),
-    fragment_spread: ($) => seq('...', $.fragment_name, optional($.directives)),
-    fragment_definition: ($) =>
+    boolean_value: (_$: any) => choice('true', 'false'),
+    null_value: (_$: any) => 'null',
+    enum_value: ($: any) => $.name,
+    list_value: ($: any) => seq('[', repeat($.value), ']'),
+    object_value: ($: any) => seq('{', repeat($.object_field), '}'),
+    object_field: ($: any) => seq($.name, ':', $.value, optional($.comma)),
+    fragment_spread: ($: any) => seq('...', $.fragment_name, optional($.directives)),
+    fragment_definition: ($: any) =>
       seq(
         'fragment',
         $.fragment_name,
@@ -314,18 +302,18 @@ export default grammar({
         optional($.directives),
         $.selection_set,
       ),
-    fragment_name: ($) => $.name,
-    inline_fragment: ($) =>
+    fragment_name: ($: any) => $.name,
+    inline_fragment: ($: any) =>
       seq(
         '...',
         optional($.type_condition),
         optional($.directives),
         $.selection_set,
       ),
-    type_condition: ($) => seq('on', $.named_type),
-    directives: ($) => repeat1($.directive),
-    directive: ($) => seq('@', $.name, optional($.arguments)),
-    directive_definition: ($) =>
+    type_condition: ($: any) => seq('on', $.named_type),
+    directives: ($: any) => repeat1($.directive),
+    directive: ($: any) => seq('@', $.name, optional($.arguments)),
+    directive_definition: ($: any) =>
       prec.right(
         1,
         seq(
@@ -339,13 +327,13 @@ export default grammar({
           $.directive_locations,
         ),
       ),
-    directive_locations: ($) =>
+    directive_locations: ($: any) =>
       choice(
         seq($.directive_locations, '|', $.directive_location),
         seq(optional('|'), $.directive_location),
       ),
-    directive_location: ($) => choice($.executable_directive_location, $.type_system_directive_location),
-    executable_directive_location: ($) =>
+    directive_location: ($: any) => choice($.executable_directive_location, $.type_system_directive_location),
+    executable_directive_location: (_$: any) =>
       choice(
         'QUERY',
         'MUTATION',
@@ -356,7 +344,7 @@ export default grammar({
         'INLINE_FRAGMENT',
         'VARIABLE_DEFINITION',
       ),
-    type_system_directive_location: ($) =>
+    type_system_directive_location: (_$: any) =>
       choice(
         'SCHEMA',
         'SCALAR',
@@ -370,13 +358,13 @@ export default grammar({
         'INPUT_OBJECT',
         'INPUT_FIELD_DEFINITION',
       ),
-    type: ($) => choice($.named_type, $.list_type, $.non_null_type),
-    named_type: ($) => $.name,
-    list_type: ($) => seq('[', $.type, ']'),
-    non_null_type: ($) => choice(seq($.named_type, '!'), seq($.list_type, '!')),
-    name: ($) => /[_A-Za-z][_0-9A-Za-z]*/,
-    comment: ($) => token(seq('#', /.*/)),
-    comma: ($) => ',',
-    description: ($) => $.string_value,
+    type: ($: any) => choice($.named_type, $.list_type, $.non_null_type),
+    named_type: ($: any) => $.name,
+    list_type: ($: any) => seq('[', $.type, ']'),
+    non_null_type: ($: any) => choice(seq($.named_type, '!'), seq($.list_type, '!')),
+    name: (_$: any) => /[_A-Za-z][_0-9A-Za-z]*/,
+    comment: (_$: any) => token(seq('#', /.*/)),
+    comma: (_$: any) => ',',
+    description: ($: any) => $.string_value,
   },
 })
